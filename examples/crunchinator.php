abec0f2703d89d5936e4728c039777041c83dcc1
maxeng <maxeng@users.noreply.github.com>
2015-03-31 15:02:23 +0900

    update details

--- examples/crunchinator.php
+++ examples/crunchinator.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="utf-8">
     <title>bookmarklet Crunchinator - PHP script to convert Javascript into bookmarklet links</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
-    <a href="https://github.com/ozh/bookmarkletgen"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+    <a href="https://github.com/maxeng/Bookmarklet-Crunchinator">
+        <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">
+    </a>
 
     <div class="container">
     <h1>bookmarklet Crunchinator</h1>
